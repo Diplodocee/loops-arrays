b1d059a8c4493c4b43296e6041d54b872e676ce6
Diplodocee <ludwa.borra@gmail.com>
2016-04-25 18:09:01 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,8 @@ while (count < 11) {
 document.write("<h1>Loops</h1>");
 document.write("<ol>" + list + "</ol>");
 
-var secondlist = "<ul>"
+var i = 0;
+var secondlist = "<ul>";
 var MyItems = [
   ["question one", "answer one"],
   ["question two", "answer two"]
@@ -19,8 +20,9 @@ var MyItems = [
   
 while (i< MyItems.length){
   secondlist+="<li>"+myItems[i]+"</li>";
+  i++;
 };
 
 secondlist+="</ul>";
 
-document.write("<li>"+myItems[i]+"</li>");
+document.write(secondlist);
