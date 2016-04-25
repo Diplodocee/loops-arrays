a7e74cc4c31ab6cd54cba710b00389e24ac29f64
Diplodocee <ludwa.borra@gmail.com>
2016-04-25 18:00:36 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 // loop
 var count = 1;
 var list = '';
-
+document.write("<h1>Loops</h1>");
 while (count < 11) {
   list += '<li>current count is: ' + count + '</li>';
+  document.write("<ol>" + list + "</ol>");
   count += 1;
   setTimeout(function() {},1250);
 }
 
 // output results
-document.write("<h1>Loops</h1>");
-document.write("<ol>" + list + "</ol>");
+
+
