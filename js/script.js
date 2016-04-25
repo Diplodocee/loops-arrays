c6eaecd28bcc9d247bc0b5f4b32c474f70701f91
Diplodocee <ludwa.borra@gmail.com>
2016-04-25 18:07:29 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
 // loop
 var count = 1;
 var list = '';
-document.write("<h1>Loops</h1>");
+
 while (count < 11) {
   list += '<li>current count is: ' + count + '</li>';
-  document.write("<ol>" + list + "</ol>");
   count += 1;
-  setTimeout(function() {},1250);
 }
 
 // output results
+document.write("<h1>Loops</h1>");
+document.write("<ol>" + list + "</ol>");
 
+var secondlist = "<ul>"
+var MyItems = [
+  ["question one", "answer one"],
+  ["question two", "answer two"]
+  ];
+  
+while (i< MyItems.length){
+  secondlist+="<li>"+myItems[i]+"</li>";
+};
 
+secondlist+="</ul>";
+
+document.write("<li>"+myItems[i]+"</li>");
